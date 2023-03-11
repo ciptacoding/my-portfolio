b0413719fc80d35c8679de2276949cbd53c6baef
ciptacoding <ciptacoding@gmail.com>
2023-03-11 22:54:34 +0700

    responsive desain mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="grid grid-cols-2 mt-12 sm:grid-cols-4 mt-12">
     <div>
-      <h1 class="text-white text-lg sm:text-2xl mb-5">Service</h1>
-      <p class="mb-5">Frontend Development</p>
+      <h1 class="text-white text-lg mb-2 sm:text-2xl mb-5">Service</h1>
+      <p class="mb-2 sm:mb-5">Frontend Development</p>
       <p class="mb-12">Fullstack Development</p>
     </div>
     <div>
-      <h1 class="text-white text-lg sm:text-2xl mb-5">About</h1>
-      <p class="mb-5">My History</p>
-      <p class="mb-12">Blogs</p>
+      <h1 class="text-white text-lg mb-2 sm:text-2xl mb-5">About</h1>
+      <p class="mb-2 sm:mb-5">My History</p>
+      <p class="mb-5 sm:mb-12">Blogs</p>
     </div>
     <div>
-      <h1 class="text-white text-lg sm:text-2xl mb-5">Information</h1>
-      <p class="mb-5">Site Map</p>
+      <h1 class="text-white text-lg mb-2 sm:text-2xl mb-5">Information</h1>
+      <p class="mb-2 sm:mb-5">Site Map</p>
       <p class="mb-12">FAQ</p>
     </div>
     <div>
-      <h1 class="text-white text-lg sm:text-2xl mb-5">Get It Touch</h1>
+      <h1 class="text-white text-lg mb-2 sm:text-2xl mb-5">Get It Touch</h1>
       <p class="mb-12">Stay connected with me and let's know more about my service and more</p>
     </div>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <section class="px-[12%] mx-auto relative">
-    <div class="bg-[#2d2d39] w-96 h-96 mx-auto rounded-full absolute z-[-1] inset-0">
+    <div class="invisible sm:visible bg-[#2d2d39] w-80 h-80 mx-auto rounded-full absolute z-[-1] inset-0">
     </div>
-    <article class="flex mt-5 relative mx-auto justify-between">
-      <div class="py-12 z-2 w-6/12">
+    <article class="w-full sm:flex mt-5 relative mx-auto justify-between">
+      <div class="w-full sm:w-6/12 py-12 z-2">
         <h1 class="text-5xl">I'm</h1>
         <p class="text-6xl text-white my-6">Cipta Dwipajaya</p>
-        <hr class="w-6/12 border-[#ffba19] border-4">
-        <div class="w-6/12 my-6">
+        <hr class="w-full sm:w-6/12 border-[#ffba19] border-4">
+        <div class="w-full sm:w-6/12 my-6">
           <p>I'm an active learner with experience and a passion for web programming, frontend development, and backend
             development. Currently, I am a student at Institute Technology and Business STIKOM Bali studying Information
             Systems.
           </p>
         </div>
       </div>
-      <div class="py-8 z-2 w-[36%]">
+      <div class="w-full mb-8 sm:w-[36%] py-8 z-2">
         <h1 class="text-xl my-4">Services</h1>
         <p class="text-2xl text-white">Let's build quality products in programming and design with my services</p>
         <p class="text-[#f2b11b] my-4">Show more
@@ -28,7 +28,7 @@
         </div>
       </div>
     </article>
-    <div class="absolute z-3 sm:left-[38%] right-[30%] top-[3rem]  h-80 w-80">
+    <div class="static max-w-fit z-3 sm:absolute left-[38%] right-[30%] top-[3rem] h-80 w-80">
       <img src="~/assets/img/IMG20230309193614-removebg2.png" alt="">
     </div>
   </section>
@@ -37,7 +37,7 @@
   <section class="bg-[#282833]">
     <article class="relative mx-[12%]">
       <div
-        class="bg-[#3f3f50] mx-auto h-[5rem] z-3 absolute inset-0 top-[-2.5rem] flex items-center justify-evenly rounded-md">
+        class="w-full absolute bg-[#3f3f50] grid mx-auto place-items-center z-3 rounded-md gap-2 h-fit py-5 grid-cols-3 top-[-8rem] sm:top-[-2.5rem] md:grid-cols-9">
         <i class="devicon-javascript-plain colored text-6xl"></i>
         <i class="devicon-vuejs-plain-wordmark colored text-6xl"></i>
         <i class="devicon-php-plain colored text-6xl"></i>
@@ -49,9 +49,9 @@
         <i class="devicon-typescript-plain colored text-6xl"></i>
       </div>
     </article>
-    <article class="flex px-[12%] mx-auto gap-10 justify-between">
-      <div class="mt-[7rem] w-[30%]">
-        <h1 class="text-white font-bold text-4xl">What Can I Do For Your Needs</h1>
+    <article class="w-full px-[12%] sm:flex px-[12%] mx-auto gap-10 justify-between">
+      <div class="w-full pt-32 sm:w-[30%] md:pt-0 mt-[7rem]">
+        <h1 class="sm:text-white font-bold text-4xl">What Can I Do For Your Needs</h1>
         <div class="w-[80%] mt-4">
           <p>It is easier to entrust the work to the exports because they are able to provide the best
             result with
@@ -72,8 +72,8 @@
           </div>
         </div>
       </div>
-      <div class="mt-[7rem] w-[40%]">
-        <div class="flex h-[100px] gap-10 justify-between">
+      <div class="mt-[7rem] w-full sm:w-[40%]">
+        <div class="flex h-[100px] justify-between gap-5 sm:gap-10">
           <img src="~/assets/img/12290877_Wavy_Tech-09_Single-08.jpg" alt="">
           <div class="self-center">
             <p class="text-white text-xl">Frontend Development</p>
@@ -85,7 +85,7 @@
         </div>
         <hr class="border-[#3f3f50] border-2 mt-3">
 
-        <div class="flex h-[100px] gap-10 justify-between mt-5">
+        <div class="flex h-[100px] justify-between mt-5 gap-5 sm:gap-10">
           <img src="~/assets/img/10332256_4412009.jpg" alt="">
           <div class="self-center">
             <p class="text-white text-xl">Backend Development</p>
@@ -97,7 +97,7 @@
         </div>
         <hr class="border-[#3f3f50] border-2 mt-3">
 
-        <div class="flex h-[100px] gap-10 justify-between mt-5">
+        <div class="flex h-[100px] justify-between mt-5 gap-5 sm:gap-10">
           <img src="~/assets/img/7118756_3426526.jpg" alt="">
           <div class="self-center">
             <p class="text-white text-xl">Fullstack Development</p>
@@ -111,7 +111,7 @@
       </div>
     </article>
   </section>
-  <section class="max-w-6xl mx-auto">
+  <section class="px-[12%] mx-auto">
     <Footer />
   </section>
 </template>
